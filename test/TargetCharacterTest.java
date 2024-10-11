35274227b71d85e3f84f4efc9e5a73a1736e94df
jiao2560 <jiao.wenbo@northeastern.edu>
2024-10-10 21:08:16 -0700

    Update TargetCharacterTest.java

--- test/TargetCharacterTest.java
+++ test/TargetCharacterTest.java
@@ -56,4 +56,16 @@ public class TargetCharacterTest {
     // Test if the health of the character is correctly returned
     assertEquals(50, targetCharacter.getHealth());
   }
+
+  @Test
+  public void testTargetCharacterParsing() {
+    String characterInfo = "50 Test Character";  // Simulating character info from a file
+    String[] parts = characterInfo.split(" ");
+    int health = Integer.parseInt(parts[0]);
+    String name = parts[1] + " " + parts[2];
+    
+    TargetCharacter parsedCharacter = new TargetCharacter(name, health, 0);
+    assertEquals("Test Character", parsedCharacter.getName());
+    assertEquals(50, parsedCharacter.getHealth());
+  }
 }
